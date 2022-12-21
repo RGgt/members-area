0c72e21c829f2c6bd4118d01f4f669f6f53d3afa
RGgt <rggt.inbox@gmail.com>
2022-12-21 21:52:32 +0200

    recreated

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-// Import the functions you need from the SDKs you need
-// import { firebaseConfig } from "./firebaseConfig";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 
 ReactDOM.createRoot(
 	document.getElementById('react-root') as HTMLElement
 ).render(
 	<React.StrictMode>
-		<App />
+		<div>Hello from REACT!!!</div>
 	</React.StrictMode>
 );
